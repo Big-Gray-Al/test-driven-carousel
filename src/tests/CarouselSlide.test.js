d10879f239d89cd4518e63a30ade2d725ea30f34
al-debian-asus <devops.sune@gmail.com>
2020-01-23 13:27:35 +0800

    :white_check_mark: Add snapshot tests for CarouselSlide

--- src/tests/CarouselSlide.test.js
+++ src/tests/CarouselSlide.test.js
@@ -17,29 +17,14 @@ describe('CarouselSlide', () => {
     );
   });
 
-  it('renders a <figure>', () => {
-    expect(wrapper.type()).toBe('figure');
+  it('renders correctly', () => {
+    wrapper.setProps({
+      description: 'Description',
+      attribution: 'Attribution',
+    });
+    expect(wrapper).toMatchSnapshot();
   });
 
-  it('renders an props.Img and a <figcaption> as children', () => {
-    expect(wrapper.childAt(0).type()).toBe(CarouselSlide.defaultProps.Img);
-    expect(wrapper.childAt(1).type()).toBe('figcaption');
-  });
-  it('passes `imgUrl` through to props.Img', () => {
-    const imgUrl = 'https://example.com/default.jpg';
-    wrapper.setProps({ imgUrl });
-    const img = wrapper.find(CarouselSlide.defaultProps.Img);
-    expect(img.prop('src')).toBe(imgUrl);
-  });
-  it('uses `description` and `attribution` as the <figcaption>', () => {
-    const description = 'A jaw-droppingly spectacular image';
-    const attribution = 'Trevor Burnham';
-    wrapper.setProps({ description, attribution });
-    expect(wrapper.find('figcaption').text()).toBe(
-      `${description} ${attribution}`
-    );
-    expect(wrapper.find('figcaption strong').text()).toBe(description);
-  });
   it('passes other props through to the <figure>', () => {
     const style = {};
     const onClick = () => {};
@@ -50,13 +35,18 @@ describe('CarouselSlide', () => {
     expect(wrapper.prop('className')).toBe(className);
   });
 });
+
 describe('Img', () => {
   let mounted;
-  const imgUrl = 'https://example.com/defualt.jpg';
+  const imgUrl = 'https://example.com/default.jpg';
 
   beforeEach(() => {
     const Img = CarouselSlide.defaultProps.Img;
     mounted = mount(<Img src={imgUrl} imgHeight={500} />);
+  });
+
+  it('renders correctly', () => {
+    expect(mounted.find('img')).toMatchSnapshot();
   });
 
   it('renders an <img> with the given src', () => {
