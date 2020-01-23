edca2ff73ff04af53d96bc3bdfc32ab97319b04c
al-debian-asus <devops.sune@gmail.com>
2020-01-23 12:32:09 +0800

    :white_check_mark: Add style tests for the <img>

--- src/Carousel.js
+++ src/Carousel.js
@@ -7,6 +7,7 @@ import CarouselSlide from './CarouselSlide';
 
 export default class Carousel extends React.PureComponent {
   static propTypes = {
+    defaultImg: CarouselSlide.propTypes.Img,
     defaultImgHeight: CarouselSlide.propTypes.imgHeight,
     slides: PropTypes.arrayOf(PropTypes.shape(CarouselSlide.propTypes))
       .isRequired,
@@ -16,6 +17,7 @@ export default class Carousel extends React.PureComponent {
   };
 
   static defaultProps = {
+    defaultImg: CarouselSlide.defaultProps.Img,
     defaultImgHeight: CarouselSlide.defaultProps.imgHeight,
   };
 
@@ -33,10 +35,11 @@ export default class Carousel extends React.PureComponent {
   };
 
   render() {
-    const { defaultImgHeight, slides, ...rest } = this.props;
+    const { defaultImg, defaultImgHeight, slides, ...rest } = this.props;
     return (
       <div {...rest}>
         <CarouselSlide
+          Img={defaultImg}
           imgHeight={defaultImgHeight}
           {...slides[this.state.slideIndex]}
         />
