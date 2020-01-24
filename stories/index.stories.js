0dec9b585649fa9c5567c1d00969013c1f0e5b3d
al-debian-asus <devops.sune@gmail.com>
2020-01-24 10:32:08 +0800

    :memo: Build storybook docs

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,11 +1,18 @@
 import React from 'react';
-
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { withKnobs, number } from '@storybook/addon-knobs';
 import Carousel from '../src/Carousel';
 import slides from '../example/slides';
 
-import { Button, Welcome } from '@storybook/react/demo';
+const stories = storiesOf('Carousel', module);
 
-storiesOf('Carousel', module).add('default', () => (
-  <Carousel slides={slides} />
+stories.addDecorator(withKnobs);
+
+stories.add('default', () => (
+  <Carousel
+    autoAdvanceDelay={number('autoAdvanceDelay', 10e3)}
+    slides={slides}
+    onIndexChange={action('onIndexChange')}
+  />
 ));
